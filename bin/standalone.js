d4f62bf90a1770fb3b60bc30226cd123830d7054
Samuel Rounce <me@samuelrounce.co.uk>
2013-03-24 15:04:47 +0000

    Watching Gent-Wevelgem... Client serving shit needs sorting

--- bin/standalone.js
+++ bin/standalone.js
@@ -1,5 +1,6 @@
 var Pusherman = require('../')
   , path = require('path')
+  , fs = require('fs')
   , express = require('express')
   , gzippo = require('gzippo')
 
@@ -13,6 +14,28 @@ app = express();
 server = require('http').createServer(app);
 pusher = new Pusherman(server);
 
+app.use(function( req, res, next ) {
+  var target = path.join(cwd, req.url);
+  var indexPath = path.join(target, 'index.html'); 
+
+  fs.stat(target, function( err, stats ) {
+    if( err ) {
+      return res.status(404).send();
+    }
+
+    if( !stats.isDirectory() ) {
+      return next();
+    }
+
+    fs.exists(indexPath, function( exists ) {
+      if( exists ) {
+        res.sendfile(indexPath);
+      } else {
+        return next();
+      }
+    });
+  });
+});
 app.use(express.directory(cwd, { icons : true }));
 app.use(gzippo.staticGzip(path.join(cwd, '/')));
 app.use(gzippo.compress());
