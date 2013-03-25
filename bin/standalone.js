a0864acfad4b8c9943232d80c4d3d9b1ce166f85
Samuel Rounce <me@samuelrounce.co.uk>
2013-03-25 10:30:08 +0000

    Added .DS_Store rule to .gitignore Moved 'client.js' to lib/ Moved bundling logic and route handler to Pusherman.HTTPHandler

--- bin/standalone.js
+++ bin/standalone.js
@@ -15,26 +15,9 @@ app = express();
 server = require('http').createServer(app);
 pusher = new Pusherman(server);
 
-!function() {
-  var engineioPath = require.resolve('engine.io-client').replace('index.js', '');
-
-  fs.readFile(path.join(engineioPath, 'engine.io.js'), { encoding : 'UTF-8' }, function( err, file ) {
-    if(err) throw new Error(err);
-
-    clientBundle += file;
-
-    fs.readFile(require.resolve('./client'), { encoding : 'UTF-8' }, function( err, file ) {
-      if(err) throw new Error(err);
-
-      clientBundle += file;
-    });
-  });
-
-}();
-
 app.configure(function() {
   app.use(express.compress());
-  app.use(app.router);
+  app.use(Pusherman.HTTPHandler); 
   app.use(function( req, res, next ) {
     var target = path.join(cwd, req.url);
     var indexPath = path.join(target, 'index.html'); 
@@ -67,10 +50,6 @@ app.configure(function() {
 });
 
 pusher.addPath(process.cwd(), '/');
-
-app.get('/.pusherman.js', function( req, res ) {
-  res.type('text/javascript').send(clientBundle);
-})
 
 server.listen(8080, function() {
   console.info('Listening on port: %d', 8080);
